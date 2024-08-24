90bcb831d8da61045a03650a4543685ed4cb9a5e
omaralian98 <omaralian9870@gmail.com>
2024-08-24 16:49:11 +0300

    .

---
+++ Helper/ApplicationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Helper;
+
+public static class ApplicationSettings
+{
+    public static bool ResetContentAfterClosing { get; set; } = false;
+
+    public static int SuggestionsOperationTitleCount { get; set; } = 4;
+
+}
