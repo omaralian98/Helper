5bb37793fd096c6c0f83559e210a3c7698d3a94b
omaralian98 <omaralian9870@gmail.com>
2024-08-29 01:34:20 +0300

    Added Inventory Made FrequentlyUsed More Generic Made the HelperDatabase more generic with the custom repositories, used entity class so that I can keep the same functionality Added some more Settings

---
+++ Helper/Data/OperationRepository.cs
@@ -0,0 +1,24 @@
+﻿using Helper.Models;
+
+namespace Helper.Data;
+
+public class OperationRepository : HelperDatabase<Operation>
+{
+    public async Task<List<string>> GetMostFrequentlyUsedOperations(int count)
+    {
+        var query = $@"
+            SELECT 
+                *
+            FROM 
+                [{nameof(Operation)}]
+            GROUP BY 
+                [{nameof(Operation.Title)}]
+            ORDER BY 
+                COUNT(*) DESC
+            LIMIT {count};";
+
+
+        var operations = await Database.QueryAsync<Operation>(query);
+        return operations.Select(op => op.Title).ToList();
+    }
+}
